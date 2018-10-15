bffc366ea1ddaf39201158fdeef86466cf74137b
Daniel Tabion <dctabion@gmail.com>
2018-10-14 23:26:50 -0500

    Part 1 working. Added sorting and displaying of records

--- app_p1_parse_files.js
+++ app_p1_parse_files.js
@@ -1,43 +1,16 @@
-const fs = require("fs");
 const parseUtils = require ('./modules/parse_utils.js');
+const { fileToRecords } = parseUtils;
 
 // Get filenames to parse.  Drop first two arguements which should be 'node' and 'app filename'.
 let filenames = process.argv;
 filenames.splice(0, 2);
-console.log('filenames: ', filenames);
 
-// Loop through command line arguments.
 let i;
 let records = [];  // will contain an array of record objects
 
-// const timeoutIn = (time) => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => resolve(time), time)
-//     })
-// }
-// timeoutIn(2000)
-//     .then(res => console.log(`Resolved in ${res/1000} seconds`))
-
-
-
-
-const fileToRecords = (filename) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile( filename, 'utf-8', (err, file) => {
-      if (err) {
-        reject(err);
-      }
-      else {
-        resolve(file);
-      }
-    });
-  })
-};
-
-console.log('filenames', filenames);
+// Prepare Promises array
 let filesToRecordsArray = [];
 for (filename of filenames) {
-  console.log('filename: ', filename);
   filesToRecordsArray.push(fileToRecords(filename));
 }
 
@@ -49,20 +22,129 @@ Promise.all(filesToRecordsArray)
 
       // Remove extra blank line from array at end
       lines.splice(lines.length - 1,1);
-      console.log('lines of input file: ', lines);
 
-      // Create array of record objects
+      // Add to array of record objects
       for (let line of lines) {
         // parse record and put into record object
         record = parseUtils.parseLine(line);
         records.push(record);
       }
     }
-    console.log('number of records: ', records.length);
-    console.log('records: ', records);
 
+    // Sort algorithm
+    function stableSort(arr, compare) {
+      var original = arr.slice(0);
+
+      arr.sort(function(a, b){
+          var result = compare(a, b);
+          return result === 0 ? original.indexOf(a) - original.indexOf(b) : result;
+      });
+
+      return arr;
+    }
+
+    // Sort the records FEMALES FIRST then LAST NAME ASCENDING
+    stableSort(records, function(a, b) {
+      return a.lastName > b.lastName ? 1 : a.lastName < b.lastName ? -1 : 0;
+    })
+
+    stableSort(records, function(a, b) {
+      return a.gender > b.gender ? 1 : a.gender < b.gender ? -1 : 0;
+    })
+
+    // Display records
+    console.log('\n\nFEMALES FIRST then LAST NAME ASCENDING');
+    console.log('======================================');
+    records.forEach(function(record) {
+       console.log(
+         record.lastName + ', ' +
+         record.firstName + ', ' +
+         record.gender + ', ' +
+         record.color + ', ' +
+         record.dob
+       );
+    });
+
+
+    // Sort the records LAST NAME ASCENDING
+    stableSort(records, function(a, b) {
+    	return a.lastName < b.lastName ? 1 : a.lastName > b.lastName ? -1 : 0;
+    })
+
+    // Display records
+    console.log('\n');
+    console.log('LAST NAME DESCENDING');
+    console.log('====================');
+    records.forEach(function(record) {
+	     console.log(
+         record.lastName + ', ' +
+         record.firstName + ', ' +
+         record.gender + ', ' +
+         record.color + ', ' +
+         record.dob
+       );
+    });
+
+    // Sort DOBs algorithm
+    function stableSort(arr, compare) {
+      var original = arr.slice(0);
+
+      arr.sort(function(a, b){
+          var result = compare(a, b);
+          return result === 0 ? original.indexOf(a) - original.indexOf(b) : result;
+      });
+
+      return arr;
+    }
+
+
+    // Sort the records DAY ASCENDING
+    stableSort(records, function(a, b) {
+      const splitDOBa = a.dob.split('/');
+      const dayA = splitDOBa[0];
+
+      const splitDOBb = b.dob.split('/');
+      const dayB = splitDOBb[0];
+
+      return dayA > dayB ? 1 : dayA < dayB ? -1 : 0;
+    });
+
+
+    // Sort the records MONTH ASCENDING
+    stableSort(records, function(a, b) {
+      const splitDOBa = a.dob.split('/');
+      const monthA = splitDOBa[1];
+
+      const splitDOBb = b.dob.split('/');
+      const monthB = splitDOBb[1];
+
+      return monthA > monthB ? 1 : monthA < monthB ? -1 : 0;
+    });
+
+    // Sort the records YEAR ASCENDING
+    stableSort(records, function(a, b) {
+      const splitDOBa = a.dob.split('/');
+      const yearA = splitDOBa[2];
+
+      const splitDOBb = b.dob.split('/');
+      const yearB = splitDOBb[2];
+
+      return yearA > yearB ? 1 : yearA < yearB ? -1 : 0;
+    });
+
+    // Display records
+    console.log('\n\nDOB ASCENDING');
+    console.log('=============');
+    records.forEach(function(record) {
+       console.log(
+         record.lastName + ', ' +
+         record.firstName + ', ' +
+         record.gender + ', ' +
+         record.color + ', ' +
+         record.dob
+       );
+    });
+
+    console.log('\n');
 
   });
-
-console.log('DONE -- hits this first because above are async calls');
-console.log('records (may be blank here because asyc file reads not done yet): ', records);
--- modules/parse_utils.js
+++ modules/parse_utils.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 module.exports = {
 
   /**
@@ -5,7 +7,7 @@ module.exports = {
    */
 
   parseLine: function(line) {
-    console.log('------in parseLine()');
+    // console.log('------in parseLine()');
     // console.log('input line: ', line);
 
     let attributes = [];
@@ -16,23 +18,23 @@ module.exports = {
     }
     // Line is PIPE delimited
     else if ( ((attributes = line.split('|')).length) == 5 ) {
-      console.log('Line is PIPE delimited');
+      // console.log('Line is PIPE delimited');
     }
     // Line is SPACE delimited
     else if ( ((attributes = line.split(' ')).length) == 5 ) {
-      console.log('Line is SPACE delimited');
+      // console.log('Line is SPACE delimited');
     }
     // Line is LINE delimited
     else if ( ((attributes = line.split(',')).length) == 5 ) {
-      console.log('Line is COMMA delimited');
+      // console.log('Line is COMMA delimited');
     }
     else {
-      console.log('Malformed input line');
+      // console.log('Malformed input line');
       return null;
     }
 
-    console.log('line: ', line);
-    console.log(attributes);
+    // console.log('line: ', line);
+    // console.log(attributes);
 
     return  {
               lastName: attributes[0],
@@ -43,13 +45,162 @@ module.exports = {
             };
   },
 
-  storeLine: function() {
-    return 42;
+
+  fileToRecords: function(filename) {
+    return new Promise((resolve, reject) => {
+      fs.readFile( filename, 'utf-8', (err, file) => {
+        if (err) {
+          reject(err);
+        }
+        else {
+          resolve(file);
+        }
+      });
+    })
+  },
+
+
+  filesToRecordsCallback: function(files) {
+    for (file of files) {
+      // Create an array where each item is a line from the file
+      let lines = file.split('\n')
+
+      // Remove extra blank line from array at end
+      lines.splice(lines.length - 1,1);
+
+      // Add to array of record objects
+      for (let line of lines) {
+        // parse record and put into record object
+        record = parseUtils.parseLine(line);
+        records.push(record);
+      }
+    }
+
+    // Sort algorithm
+    function stableSort(arr, compare) {
+      var original = arr.slice(0);
+
+      arr.sort(function(a, b){
+          var result = compare(a, b);
+          return result === 0 ? original.indexOf(a) - original.indexOf(b) : result;
+      });
+
+      return arr;
+    }
+
+    // Sort the records FEMALES FIRST then LAST NAME ASCENDING
+    stableSort(records, function(a, b) {
+      return a.lastName > b.lastName ? 1 : a.lastName < b.lastName ? -1 : 0;
+    })
+
+    stableSort(records, function(a, b) {
+      return a.gender > b.gender ? 1 : a.gender < b.gender ? -1 : 0;
+    })
+
+    // Display records
+    console.log('\n\nFEMALES FIRST then LAST NAME ASCENDING');
+    console.log('======================================');
+    records.forEach(function(record) {
+       console.log(
+         record.lastName + ', ' +
+         record.firstName + ', ' +
+         record.gender + ', ' +
+         record.color + ', ' +
+         record.dob
+       );
+    });
+
+
+    // Sort the records LAST NAME ASCENDING
+    stableSort(records, function(a, b) {
+      return a.lastName < b.lastName ? 1 : a.lastName > b.lastName ? -1 : 0;
+    })
+
+    // Display records
+    console.log('\n');
+    console.log('LAST NAME DESCENDING');
+    console.log('====================');
+    records.forEach(function(record) {
+       console.log(
+         record.lastName + ', ' +
+         record.firstName + ', ' +
+         record.gender + ', ' +
+         record.color + ', ' +
+         record.dob
+       );
+    });
+
+    // Sort DOBs algorithm
+    function stableSort(arr, compare) {
+      var original = arr.slice(0);
+
+      arr.sort(function(a, b){
+          var result = compare(a, b);
+          return result === 0 ? original.indexOf(a) - original.indexOf(b) : result;
+      });
+
+      return arr;
+    }
+
+
+    // Sort the records DAY ASCENDING
+    stableSort(records, function(a, b) {
+      const splitDOBa = a.dob.split('/');
+      const dayA = splitDOBa[0];
+
+      const splitDOBb = b.dob.split('/');
+      const dayB = splitDOBb[0];
+
+      return dayA > dayB ? 1 : dayA < dayB ? -1 : 0;
+    });
+
+
+    // Sort the records MONTH ASCENDING
+    stableSort(records, function(a, b) {
+      const splitDOBa = a.dob.split('/');
+      const monthA = splitDOBa[1];
+
+      const splitDOBb = b.dob.split('/');
+      const monthB = splitDOBb[1];
+
+      return monthA > monthB ? 1 : monthA < monthB ? -1 : 0;
+    });
+
+    // Sort the records YEAR ASCENDING
+    stableSort(records, function(a, b) {
+      const splitDOBa = a.dob.split('/');
+      const yearA = splitDOBa[2];
+
+      const splitDOBb = b.dob.split('/');
+      const yearB = splitDOBb[2];
+
+      return yearA > yearB ? 1 : yearA < yearB ? -1 : 0;
+    });
+
+    // Display records
+    console.log('\n\nDOB ASCENDING');
+    console.log('=============');
+    records.forEach(function(record) {
+       console.log(
+         record.lastName + ', ' +
+         record.firstName + ', ' +
+         record.gender + ', ' +
+         record.color + ', ' +
+         record.dob
+       );
+    });
+
+    console.log('\n');
+
   },
 
   /**
    * Sort functions
    */
+
+   sortLastName: function() {
+     return 42;
+   },
 
   sortGender: function() {
     return 42;
@@ -59,8 +210,6 @@ module.exports = {
     return 42;
   },
 
-  sortLastName: function() {
-    return 42;
-  }
+
 
 };
