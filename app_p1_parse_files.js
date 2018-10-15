9bc2f31e8eb51ccce48b1a4d54d048c83f940c92
Daniel Tabion <dctabion@gmail.com>
2018-10-14 20:03:07 -0500

    refactored file parsing with promises so action (sorting) is taken only after all files are read.

--- app_p1_parse_files.js
+++ app_p1_parse_files.js
@@ -8,31 +8,61 @@ console.log('filenames: ', filenames);
 
 // Loop through command line arguments.
 let i;
-let records = [];
+let records = [];  // will contain an array of record objects
 
-// Loop through all files from command line arguments
-for (i = 0; i < filenames.length; i++) {
-  // Read in file
-  fs.readFile(filenames[i], 'utf-8', (err, file) => {
-    // create an array where each item is a line from the file
-    let lines = file.split('\n')
+// const timeoutIn = (time) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => resolve(time), time)
+//     })
+// }
+// timeoutIn(2000)
+//     .then(res => console.log(`Resolved in ${res/1000} seconds`))
 
-    // Remove extra blank line from array at end
-    lines.splice(lines.length - 1,1);
-    console.log('lines of input file: ', lines);
 
-    // push each record object into an array
-    for (let line of lines) {
-      // parse record and put into record object
-      record = parseUtils.parseLine(line);
-      records.push(record);
+
+
+const fileToRecords = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile( filename, 'utf-8', (err, file) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(file);
+      }
+    });
+  })
+};
+
+console.log('filenames', filenames);
+let filesToRecordsArray = [];
+for (filename of filenames) {
+  console.log('filename: ', filename);
+  filesToRecordsArray.push(fileToRecords(filename));
+}
+
+Promise.all(filesToRecordsArray)
+  .then((files) => {
+    for (file of files) {
+      // Create an array where each item is a line from the file
+      let lines = file.split('\n')
+
+      // Remove extra blank line from array at end
+      lines.splice(lines.length - 1,1);
+      console.log('lines of input file: ', lines);
+
+      // Create array of record objects
+      for (let line of lines) {
+        // parse record and put into record object
+        record = parseUtils.parseLine(line);
+        records.push(record);
+      }
     }
-
     console.log('number of records: ', records.length);
     console.log('records: ', records);
-  });
 
-}
+
+  });
 
 console.log('DONE -- hits this first because above are async calls');
 console.log('records (may be blank here because asyc file reads not done yet): ', records);
